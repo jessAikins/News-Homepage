44bb45af79ace13df20ff1982d110a04010c1ee0
jessAikins <148063466+jessAikins@users.noreply.github.com>
2025-12-12 10:05:42 +0000

    Add files via upload

---
+++ web/script.js
@@ -0,0 +1,9 @@
+function openMenu () {
+    document.getElementById("sidebar").classList.add("active")
+    document.getElementById("overlay").classList.add("active")
+}
+
+function closeMenu () {
+    document.getElementById("sidebar").classList.remove("active")
+    document.getElementById("overlay").classList.remove("active")
+}
