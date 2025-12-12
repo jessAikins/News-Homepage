2b4274d179538920dabfadb68bcafc34dbc16b6a
jessAikins <148063466+jessAikins@users.noreply.github.com>
2025-12-12 10:18:06 +0000

    Delete web directory

--- web/script.js
+++
@@ -1,9 +0,0 @@
-function openMenu () {
-    document.getElementById("sidebar").classList.add("active")
-    document.getElementById("overlay").classList.add("active")
-}
-
-function closeMenu () {
-    document.getElementById("sidebar").classList.remove("active")
-    document.getElementById("overlay").classList.remove("active")
-}
